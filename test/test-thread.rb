913998f8870ea3e29011edebb60558acd98226d8
rocky <rocky@sanchez.(none)>
2009-06-24 23:43:09 -0400

    Thread::Frame#new() now takes a Thread object and returns a fully-initialized object now. Add Thread::Frame#thread.

---
+++ test/test-thread.rb
@@ -0,0 +1,8 @@
+require 'test/unit'
+require_relative File.join('..', 'ext', 'thread_frame')
+
+class TestThread < Test::Unit::TestCase
+  def test_basic
+    assert_equal(Thread::Frame.current.thread, Thread::current)
+  end
+end
