e0840daa11fc3cba824d6340c820a22ed52325ef
rocky <rockyb@rubyforge.org>
2009-07-20 20:52:22 -0400

    Add ThreadFrame#type to indicate what kind of frame we got. Todo: fix ThreadFrame#prev not to skip over C frames and do a fixup of those.

--- ext/thread_frame.c
+++ ext/thread_frame.c
@@ -31,8 +31,6 @@ typedef struct
 VALUE rb_cThreadFrame;       /* ThreadFrame class */
 VALUE rb_eThreadFrameError;  /* Error raised on invalid frames. */
 
-static VALUE thread_frame_invalid_internal(thread_frame_t *tf);
-
 /* 
    Allocate a RubyVM::ThreadFrame used by new. Less common than
    thread_frame_t_alloc().
@@ -57,6 +55,19 @@ thread_frame_t_alloc(VALUE tfval)
     return tf;
 }
 
+static VALUE
+thread_frame_invalid_internal(thread_frame_t *tf)
+{
+    int cmp;
+    if (RUBY_VM_CONTROL_FRAME_STACK_OVERFLOW_P(tf->th, tf->cfp))
+	return Qtrue;
+    cmp = (0 == memcmp(tf->signature1, &(tf->cfp->iseq), 
+		       sizeof(tf->signature1)) &&
+	   0 == memcmp(tf->signature2, &(tf->cfp->proc), 
+		       sizeof(tf->signature2)));
+    return cmp ? Qfalse : Qtrue;
+}
+    
 #define SAVE_FRAME(TF, TH)						\
     tf->th = TH;							\
     tf->cfp = thread_context_frame(tf->th);				\
@@ -67,31 +78,6 @@ thread_frame_t_alloc(VALUE tfval)
     rb_thread_t *th; \
     GetThreadPtr(thval, th)
 
-
-/*
- *  call-seq:
- *     RubyVM::ThreadFrame.new(thread)          => thread_frame_object
- *
- *  Returns an RubyVM::ThreadFrame object which can contains dynamic frame
- *  information. Don't use this directly. Instead use one of the 
- *  class methods.
- *
- *     ThreadFrame::VERSION               => 0.1 
- *     ThreadFrame::current.flag          => 72
- *     ThreadFrame::current.method_class  => Comparable (?)
- *     ThreadFrame::current.proc          => false
- *     ThreadFrame::current.self          => 'main'
- */
-static VALUE
-thread_frame_initialize(VALUE tfval, VALUE thval)
-{
-    thread_frame_t *tf = thread_frame_t_alloc(tfval);
-    GET_THREAD_PTR ;
-    memset(tf, 0, sizeof(thread_frame_t));
-    DATA_PTR(tfval) = tf;
-    SAVE_FRAME(tf, th) ;
-    return tfval;
-}
 
 /*
  *  call-seq:
@@ -140,37 +126,6 @@ thread_frame_s_current(VALUE klass)
     thread_frame_t *tf; \
     Data_Get_Struct(klass, thread_frame_t, tf)
 
-/*
- *  call-seq:
- *     RubyVM::ThreadFrame#binding   => binding
- *
- *  Returns a binding for a given thread frame.
- */
-static VALUE
-thread_frame_binding(VALUE klass)
-{
-    THREAD_FRAME_SETUP ;
-    if (Qtrue == thread_frame_invalid_internal(tf))
-	rb_raise(rb_eThreadFrameError, "invalid frame");
-    else
-	return rb_binding_frame_new(tf->th, tf->cfp);
-    /* NOTREACHED */
-    return Qnil;
-}
-
-/*
- *  call-seq:
- *     RubyVM::ThreadFrame#thread   => thread
- *
- *  Returns the thread object for the thread frame.
- */
-static VALUE
-thread_frame_thread(VALUE klass)
-{
-    THREAD_FRAME_SETUP ;
-    return tf->th->self;
-}
-
 #define THREAD_FRAME_FIELD_METHOD(FIELD)	\
 static VALUE					\
 thread_frame_##FIELD(VALUE klass)		\
@@ -178,50 +133,6 @@ thread_frame_##FIELD(VALUE klass)		\
     THREAD_FRAME_SETUP ;			\
     return tf->cfp->FIELD;			\
 }
-
-/*
- *  call-seq:
- *     Thread#method  => String
- * 
- * Returns the method associated with the frame or nil of none.
- * ThreadFrameError can be raised if the threadframe
- * object is no longer valid.
- */
-static VALUE
-thread_frame_method(VALUE klass)
-{
-    THREAD_FRAME_SETUP ;			\
-    if (Qtrue == thread_frame_invalid_internal(tf))
-	rb_raise(rb_eThreadFrameError, "invalid frame");
-    if (RUBY_VM_NORMAL_ISEQ_P(tf->cfp->iseq)) 
-	return tf->cfp->iseq->name;
-    else if (RUBYVM_CFUNC_FRAME_P(tf->cfp))
-	return rb_str_new2(rb_id2name(tf->cfp->method_id));
-    return Qnil;
-}
-
-
-/*
- *  call-seq:
- *     Thread#pc_offset  => Fixnum
- * 
- * Returns the offset inside the iseq or "program-counter offset" or -1
- * If invalid/unstarted. ThreadFrameError can be raised if the threadframe
- * object is no longer valid.
- */
-static VALUE
-thread_frame_pc_offset(VALUE klass)
-{
-    int pc = -1;
-    THREAD_FRAME_SETUP ;
-    if (Qtrue == thread_frame_invalid_internal(tf))
-	rb_raise(rb_eThreadFrameError, "invalid frame");
-    if (RUBY_VM_NORMAL_ISEQ_P(tf->cfp->iseq)) {
-	pc = tf->cfp->pc - tf->cfp->iseq->iseq_encoded;
-    }
-    return INT2FIX(pc);
-}
-
 
 #ifdef NO_reg_pc
 /*
@@ -250,9 +161,6 @@ thread_frame_set_pc_offset(VALUE klass, VALUE offset_val)
 #endif
 
 THREAD_FRAME_FIELD_METHOD(flag) ;
-THREAD_FRAME_FIELD_METHOD(method_class) ;
-THREAD_FRAME_FIELD_METHOD(proc) ;
-THREAD_FRAME_FIELD_METHOD(self) ;
 
 static VALUE
 thread_frame_prev_common(rb_control_frame_t *prev_cfp, rb_thread_t *th)
@@ -274,24 +182,6 @@ thread_frame_prev_common(rb_control_frame_t *prev_cfp, rb_thread_t *th)
 
 /*
  *  call-seq:
- *     RubyVM::ThreadFrame#prev           => thread_frame_object
- *
- *  Returns a RubyVM::ThreadFrame object for the frame prior to the
- *  ThreadFrame object or nil if there is none.
- *
- */
-static VALUE
-thread_frame_prev(VALUE klass)
-{
-    rb_control_frame_t *prev_cfp;
-    THREAD_FRAME_SETUP ;
-    prev_cfp = RUBY_VM_PREVIOUS_CONTROL_FRAME(tf->cfp);
-    prev_cfp = rb_vm_get_ruby_level_next_cfp(tf->th, prev_cfp);
-    return thread_frame_prev_common(prev_cfp, tf->th);
-}
-
-/*
- *  call-seq:
  *     RubyVM::ThreadFrame#prev(thread)     => threadframe_object
  *
  *  Returns a RubyVM::ThreadFrame for the frame prior to the
@@ -306,6 +196,115 @@ thread_frame_thread_prev(VALUE klass, VALUE thval)
     prev_cfp = RUBY_VM_PREVIOUS_CONTROL_FRAME(th->cfp);
     return thread_frame_prev_common(prev_cfp, th);
 }
+
+/*
+ *  call-seq:
+ *     RubyVM::ThreadFrame#binding   => binding
+ *
+ *  Returns a binding for a given thread frame.
+ */
+static VALUE
+thread_frame_binding(VALUE klass)
+{
+    THREAD_FRAME_SETUP ;
+    if (Qtrue == thread_frame_invalid_internal(tf))
+	rb_raise(rb_eThreadFrameError, "invalid frame");
+    else
+	return rb_binding_frame_new(tf->th, tf->cfp);
+    /* NOTREACHED */
+    return Qnil;
+}
+
+/*
+ *  call-seq:
+ *     RubyVM::ThreadFrame.new(thread)          => thread_frame_object
+ *
+ *  Returns an RubyVM::ThreadFrame object which can contains dynamic frame
+ *  information. Don't use this directly. Instead use one of the 
+ *  class methods.
+ *
+ *     ThreadFrame::VERSION               => 0.1 
+ *     ThreadFrame::current.flag          => 72
+ *     ThreadFrame::current.method_class  => Comparable (?)
+ *     ThreadFrame::current.proc          => false
+ *     ThreadFrame::current.self          => 'main'
+ */
+static VALUE
+thread_frame_initialize(VALUE tfval, VALUE thval)
+{
+    thread_frame_t *tf = thread_frame_t_alloc(tfval);
+    GET_THREAD_PTR ;
+    memset(tf, 0, sizeof(thread_frame_t));
+    DATA_PTR(tfval) = tf;
+    SAVE_FRAME(tf, th) ;
+    return tfval;
+}
+
+/*
+ * call-seq:
+ *    RubyVM::ThreadFrame#invalid? -> Boolean
+ *
+ * Returns true if the frame is no longer valid. On the other hand,
+ * since the test we use is weak, returning false might not mean the
+ * frame is valid, just that we can't disprove that it is not invalid.
+ * 
+ * It is suggested that frames are used in a way that ensures they will
+ * be valid. In particular frames should have local scope and frames to 
+ * threads other than the running one should be stopped while the frame 
+ * variable is active.
+ */
+static VALUE
+thread_frame_invalid(VALUE klass)
+{
+    THREAD_FRAME_SETUP ;
+    return thread_frame_invalid_internal(tf);
+}
+
+/*
+ *  call-seq:
+ *     Thread#method  => String
+ * 
+ * Returns the method associated with the frame or nil of none.
+ * ThreadFrameError can be raised if the threadframe
+ * object is no longer valid.
+ */
+static VALUE
+thread_frame_method(VALUE klass)
+{
+    THREAD_FRAME_SETUP ;			\
+    if (Qtrue == thread_frame_invalid_internal(tf))
+	rb_raise(rb_eThreadFrameError, "invalid frame");
+    if (RUBY_VM_NORMAL_ISEQ_P(tf->cfp->iseq)) 
+	return tf->cfp->iseq->name;
+    else if (RUBYVM_CFUNC_FRAME_P(tf->cfp))
+	return rb_str_new2(rb_id2name(tf->cfp->method_id));
+    return Qnil;
+}
+
+
+THREAD_FRAME_FIELD_METHOD(method_class) ;
+
+/*
+ *  call-seq:
+ *     Thread#pc_offset  => Fixnum
+ * 
+ * Returns the offset inside the iseq or "program-counter offset" or -1
+ * If invalid/unstarted. ThreadFrameError can be raised if the threadframe
+ * object is no longer valid.
+ */
+static VALUE
+thread_frame_pc_offset(VALUE klass)
+{
+    int pc = -1;
+    THREAD_FRAME_SETUP ;
+    if (Qtrue == thread_frame_invalid_internal(tf))
+	rb_raise(rb_eThreadFrameError, "invalid frame");
+    if (RUBY_VM_NORMAL_ISEQ_P(tf->cfp->iseq)) {
+	pc = tf->cfp->pc - tf->cfp->iseq->iseq_encoded;
+    }
+    return INT2FIX(pc);
+}
+
 
 /*
  *  call-seq:
@@ -327,6 +326,28 @@ thread_frame_iseq(VALUE klass)
     RDATA(rb_iseq)->data = iseq;
     return rb_iseq;
 }
+
+/*
+ *  call-seq:
+ *     RubyVM::ThreadFrame#prev           => thread_frame_object
+ *
+ *  Returns a RubyVM::ThreadFrame object for the frame prior to the
+ *  ThreadFrame object or nil if there is none.
+ *
+ */
+static VALUE
+thread_frame_prev(VALUE klass)
+{
+    rb_control_frame_t *prev_cfp;
+    THREAD_FRAME_SETUP ;
+    prev_cfp = RUBY_VM_PREVIOUS_CONTROL_FRAME(tf->cfp);
+    /* FIXME: deal with C frames. */
+    prev_cfp = rb_vm_get_ruby_level_next_cfp(tf->th, prev_cfp);
+    return thread_frame_prev_common(prev_cfp, tf->th);
+}
+
+THREAD_FRAME_FIELD_METHOD(proc) ;
+THREAD_FRAME_FIELD_METHOD(self) ;
 
 /*
  * call-seq:
@@ -382,41 +403,40 @@ thread_frame_source_location(VALUE klass)
 }
 
 /*
- * call-seq:
- *    RubyVM::ThreadFrame#invalid? -> Boolean
+ *  call-seq:
+ *     RubyVM::ThreadFrame#thread   => thread
  *
- * Returns true if the frame is no longer valid. On the other hand,
- * since the test we use is weak, returning false might not mean the
- * frame is valid, just that we can't disprove that it is not invalid.
- * 
- * It is suggested that frames are used in a way that ensures they will
- * be valid. In particular frames should have local scope and frames to 
- * threads other than the running one should be stopped while the frame 
- * variable is active.
+ *  Returns the thread object for the thread frame.
  */
 static VALUE
-thread_frame_invalid(VALUE klass)
+thread_frame_thread(VALUE klass)
 {
     THREAD_FRAME_SETUP ;
-    return thread_frame_invalid_internal(tf);
+    return tf->th->self;
 }
 
+/*
+ *  call-seq:
+ *     Thread#type  => String one of: "C", "Ruby", or "unknown"
+ * 
+ * Returns the kind of frame. Is either "C", "Ruby" or "unknown".
+ */
 static VALUE
-thread_frame_invalid_internal(thread_frame_t *tf)
+thread_frame_type(VALUE klass)
 {
-    int cmp;
-    if (RUBY_VM_CONTROL_FRAME_STACK_OVERFLOW_P(tf->th, tf->cfp))
-	return Qtrue;
-    cmp = (0 == memcmp(tf->signature1, &(tf->cfp->iseq), 
-		       sizeof(tf->signature1)) &&
-	   0 == memcmp(tf->signature2, &(tf->cfp->proc), 
-		       sizeof(tf->signature2)));
-    return cmp ? Qfalse : Qtrue;
+    THREAD_FRAME_SETUP ;			\
+    if (RUBY_VM_NORMAL_ISEQ_P(tf->cfp->iseq)) 
+	return rb_str_new2("Ruby");
+    else if (RUBYVM_CFUNC_FRAME_P(tf->cfp))
+	return rb_str_new2("C");
+    else
+	return rb_str_new2("unknown");
+    /* NOTREACHED */
+    return Qnil;
 }
-    
+
 
 extern VALUE rb_cThread;
-
 extern VALUE proc_iseq(VALUE self);
 
 #define RB_DEFINE_FRAME_METHOD(FIELD, ARGC)					\
@@ -451,6 +471,7 @@ Init_thread_frame(void)
     RB_DEFINE_FRAME_METHOD(source_container, 0);
     RB_DEFINE_FRAME_METHOD(source_location, 0);
     RB_DEFINE_FRAME_METHOD(thread, 0);
+    RB_DEFINE_FRAME_METHOD(type, 0);
 
 #ifdef NO_reg_pc
     rb_define_method(rb_cThreadFrame, "pc_offset=", 
