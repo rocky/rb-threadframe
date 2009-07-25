e71f71d5d281d3fda2c91ca937178509c3ff71b6
rocky <rockyb@rubyforge.org>
2009-07-24 20:30:53 -0400

    Was SEGV'ing in arity on C-style frames.

---
+++ lib/iseq_extra.rb
@@ -0,0 +1,23 @@
+require_relative File.join(%w(.. ext thread_frame))
+class RubyVM::InstructionSequence
+  def format_args
+    if iseq
+      args = 0.upto(iseq.arity-1).map do |i| 
+        iseq.local_name(i)
+      end.join(', ')
+
+      last = iseq.local_table_size-1
+      if last > iseq.arity
+        args += '; ' + args = iseq.arity.upto(last).map do |i| 
+          iseq.local_name(i)
+        end.join(', ')
+      end
+    else
+      args = '?'
+    end
+  end
+end
+if __FILE__ == $0
+  # Demo it.
+end
+
