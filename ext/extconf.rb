141556e7fb0a8628c302f8ceef11d3a6f4c7845e
rocky <rockyb@rubyforge.org>
2009-07-29 16:28:56 -0400

    Finally figured out how to customize the mkmf Makefile WITHOUT checking in those options into version control

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,8 @@
 require "mkmf"
+
+config_file = File.join(File.dirname(__FILE__), 'config_options')
+load config_file if File.exist?(config_file)
+
 # Temporary: to turn off optimization
 # $CFLAGS='-fno-strict-aliasing -g -fPIC'
 create_makefile("thread_frame")
