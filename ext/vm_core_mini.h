acb7ec4010411147ad5fc97bc282ce2416ef573f
rocky <rockyb@rubyforge.org>
2009-08-14 00:55:28 -0400

    Conflicted merge.

--- ext/vm_core_mini.h
+++ ext/vm_core_mini.h
@@ -56,11 +56,7 @@
 #define GetISeqPtr(obj, ptr) \
   GetCoreDataFromValue(obj, rb_iseq_t, ptr)
 
-<<<<<<< HEAD:ext/vm_core_mini.h
 /* Opaque types (for now at least) */
-=======
-/* Opaque types (for now) */
->>>>>>> 05185f1fd1999c92ecef9cab96ddd2944a8ceb2b:ext/vm_core_mini.h
 typedef struct iseq_catch_table_entry iseq_catch_table_entry_t;
 typedef struct node NODE;
 
@@ -214,7 +210,7 @@ typedef struct rb_vm_struct {
     struct rb_thread_struct *main_thread;
     struct rb_thread_struct *running_thread;
 
-    struct st_table *living_threads;
+    st_table *living_threads;
     VALUE thgroup_default;
 
     int running;
