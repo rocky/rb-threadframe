9eb8ebfc272cdf59d283a2b0f499e11a9b6c8290
rocky <rockyb@rubyforge.org>
2010-07-23 12:18:29 -0400

    01-get-sourceline.patch: rb_vm_get_sourceline returns the wrong line number (unconditionally set to 0) when the PC is 0.  This patch corrects that.

--- patches/patch-ruby.sh
+++ patches/patch-ruby.sh
@@ -9,14 +9,15 @@ patchfile=${1:-'head'}
 case $patchfile in
     1.9.3 | head | trunk )
 	for file in \
-	    00-extern-access.patch \
-	    01-disasm-insns.patch  \
-	    02-iseq-access.patch   \
-	    03-iseq-create.patch   \
-	    04-C-argc.patch        \
-	    05-brkpt.patch         \
-	    06-raise-msg.patch     \
-	    07-trace_func.patch
+	    00-extern-access.patch  \
+	    01-get-sourceline.patch \
+	    02-disasm-insns.patch  \
+	    03-iseq-access.patch   \
+	    04-iseq-create.patch   \
+	    05-C-argc.patch        \
+	    06-brkpt.patch         \
+	    07-raise-msg.patch     \
+	    08-trace_func.patch
 	do 
 	    echo -- Applying patches in ${dirname}/trunk/$file ...
 	    patch -p0 < ${dirname}/trunk/$file
