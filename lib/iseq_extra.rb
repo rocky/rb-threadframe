5df268e252da693a9491eb0899be5efcfa3c5ef7
rocky <rockyb@rubyforge.org>
2010-11-12 23:34:41 -0500

    Add routines for finding instruction sequences and offsets. In particular iseq#lines() iseq#find_iseq_with_line(),               iseq#locate_line_with_children()

--- lib/iseq_extra.rb
+++ lib/iseq_extra.rb
@@ -60,6 +60,74 @@ class RubyVM::InstructionSequence
     Digest::SHA1.hexdigest(encoded)
   end
 
+
+  ##
+  # Locates the instruction address (IP) of the first instruction on
+  # the specified line or nil if no match for the specified line is
+  # found.
+  #
+  # @return [Fixnum, NilClass] returns
+  #   nil if nothing is found, else the first IP for the line
+  def locate_line(line)
+    offsetlines.each_pair do |ip, val|
+      return ip if val.member?(line)
+    end
+  end
+
+  # iseq and instruction address (IP) of the first instruction on
+  # the specified line. This method recursively examines child
+  # compiled methods until an exact match for the searched line is
+  # found.  It returns both the matching CompiledMethod and the IP
+  # of the first instruction on the requested line, or nil if no
+  # match for the specified line is found.
+  #
+  # @return [(RubyVM::InstructionSequence, Fixnum), NilClass] returns
+  #   nil if nothing is found, else an array of size 2 containing the method
+  #   the line was found in and the IP pointing there.
+  def locate_line_with_children(line)
+    iseq = self
+    ip = iseq.locate_line(line)
+    return iseq, ip if ip
+    
+    # Didn't find line in this iseq, so check if a contained
+    # InstructionSequence encompasses the line searched for
+    child_iseqs.each do |child_iseq|
+      next if child_iseq.equal? iseq
+      if res = locate_line_with_children(line, child_iseq)
+        return res
+      end
+    end
+
+    # No child method is a match - fail
+    return nil
+  end
+
+  def lines
+    offsetlines.values.flatten.uniq
+  end
+
+  # Returns an InstructionSequence for the specified line. We search the
+  # current method +meth+ and then up the parent scope.  If we hit
+  # the top and we can't find +line+ that way, then we
+  # reverse the search from the top and search down. This will add
+  # all siblings of ancestors of +meth+.
+  def find_iseq_with_line(line)
+
+    lines = self.lines
+    iseq = self
+    until lines.member?(line) do
+      child_iseq = iseq
+      iseq = iseq.parent
+      unless iseq
+        # child is the top-most scope. Search down from here.
+        pair = child_iseq.locate_line_with_children(line)
+        ## pair = iseq.locate_line(line)
+        return pair ? pair[0] : nil
+      end
+      lines = iseq.lines
+    end
+    return iseq
+  end
 end
 
 if __FILE__ == $0
@@ -85,5 +153,20 @@ if __FILE__ == $0
     end
   end
   show_type
+  puts '-' * 40
+  
+  line = __LINE__
+  def find_line(line) # :nodoc
+    tf = RubyVM::ThreadFrame.current
+    puts "find_line has lines: #{tf.iseq.lines}"
+    p tf.iseq.find_iseq_with_line(line)
+  end
+
+  tf = RubyVM::ThreadFrame.current
+  puts tf.iseq.disassemble
+  puts("offset %d above should be at line %d" % 
+       [tf.iseq.locate_line(line), line])
+  find_line(line)
+  p tf.iseq.find_iseq_with_line(line+2)
 end
 
---
+++ test/unit/test-lib-iseq.rb
@@ -0,0 +1,46 @@
+require 'test/unit'
+require_relative '../../ext/thread_frame'
+require_relative '../../lib/thread_frame'
+
+$global_test_line = __LINE__
+
+class TestLibISeq < Test::Unit::TestCase
+
+  TEST_LINE = __LINE__
+
+  def test_sha1
+    iseq1 = RubyVM::ThreadFrame::current.iseq
+    iseq2 = RubyVM::ThreadFrame::current.iseq
+    assert_equal(iseq1.sha1, iseq2.sha1, 
+                 "SHA1 for same threadframe should match")
+  end
+
+  def test_lines
+    line = __LINE__
+    iseq = RubyVM::ThreadFrame::current.iseq
+    assert_equal((line-1..__LINE__+2).to_a, iseq.lines,
+                 "lines of test_lines() don't match")
+  end
+
+  def test_iseq_with_line
+    # FIXME: We get a more stringent test if we test of offset.
+    line = __LINE__
+    def find_line(line) # :nodoc
+      tf = RubyVM::ThreadFrame.current
+      assert tf.iseq.find_iseq_with_line(line)
+    end
+    tf = RubyVM::ThreadFrame.current
+    find_line(line)
+    line2 = nil
+    1.times do 
+      line2 = __LINE__
+    end
+    find_line(line2)
+    find_line(TEST_LINE)
+    find_line($global_test_line)
+  end
+
+end
+
+# We want to double-check we didn't mess up any pointers somewhere.
+at_exit { GC.start  }
