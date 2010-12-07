eac57cab3b7d21e7bc51484509c3801dcdf01707
rocky <rockyb@rubyforge.org>
2010-12-06 20:45:51 -0500

    Small tweaks on how extension customization works.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,9 +4,11 @@ fail "You need to install a threadframe-patched Ruby.
 See http://github.com/rocky/rb-threadframe/wiki/How-to-Install" unless 
   RbConfig::CONFIG.member?('rb-threadframe')
 
-config_file = File.join(File.dirname(__FILE__), 'config_options')
+# Allow use customization of compile options. For example, the 
+# following lines could be put in config_options:
+# CONFIG['optflags'] = ''   # Or -O3
+# CONFIG['debugflags'] = '-g3 -ggdb' 
+config_file = File.join(File.dirname(__FILE__), 'config_options.rb')
 load config_file if File.exist?(config_file)
 
-# Temporary: to turn off optimization
-# $CFLAGS='-fno-strict-aliasing -g -fPIC'
 create_makefile("thread_frame")
