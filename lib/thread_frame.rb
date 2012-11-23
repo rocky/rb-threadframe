db670605445f6e0a51c314396693b7e6f802f39b
rocky <rockyb@rubyforge.org>
2012-11-22 21:35:52 -0500

    exec_event_tracing added to broken-out patches. Make sure iseq_extra is included for 1.9.3

--- lib/thread_frame.rb
+++ lib/thread_frame.rb
@@ -1,8 +1,8 @@
 # Boilerplate to pull shared object and helper routines.
 if RUBY_VERSION == '1.9.2'
   require_relative '../ext/thread_frame'
-  require_relative 'iseq_extra'
 end
+require_relative 'iseq_extra'
 
 RubyVM::ThreadFrame = RubyVM::Frame
 class Thread
